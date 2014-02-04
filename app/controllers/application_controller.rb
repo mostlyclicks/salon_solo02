6acb44b6cd4f3e59154ce629e4596798774e7fcf
Carlos Interone <carlos@mostlyclicks.com>
2014-02-04 16:28:31 -0600

    wire up engines to front page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :list_active_promos
+  before_filter :list_services_excerpts
+  before_filter :list_product_brands
+
+  protected
+
+    def list_active_promos
+      @promos = (Refinery::Promos::Promo.where promo_status: "1").limit(2)
+    end
+
+    def list_services_excerpts
+      @services = (Refinery::Services::Service).all
+    end
+
+    def list_product_brands
+      @products = (Refinery::Products::Product).all
+    end
+
 end
