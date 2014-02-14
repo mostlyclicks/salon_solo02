0f8c825971329826d189cb63962488de88b5326f
Carlos Interone <carlos@mostlyclicks.com>
2014-02-14 11:24:06 -0600

    work on layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   def main_nav_nobg
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
     presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
-    presenter.list_tag = "ul class=\"nav navbar-nav pull-right pull-left-xs\""
+    presenter.list_tag = "ul class=\"nav navbar-nav navbar-left\""
     presenter.max_depth = 0
     presenter
   end
@@ -18,7 +18,7 @@ module ApplicationHelper
   def main_navbar_default
     presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
     presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"main-nav-collapse\""
-    presenter.list_tag = "ul class=\"nav navbar-nav\""
+    presenter.list_tag = "ul class=\"nav navbar-nav \""
     presenter.max_depth = 0
     presenter
   end
