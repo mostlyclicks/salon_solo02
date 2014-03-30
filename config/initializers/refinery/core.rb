55fd7fa9ac1abd7763b888bc033046701f4f195c
Carlos Interone <carlos@mostlyclicks.com>
2014-03-30 11:36:14 -0500

    change title and meta description

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,7 +20,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "Salon Solo - La Crosse/Onalaska WI - (608) 782-1243"
+  config.site_name = "Salon Solo - Salon and spa services in La Crosse and Onalaska Wisconsin - (608) 782-1243"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
